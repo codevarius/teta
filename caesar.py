78709e97e3691ab09122e36afd08e21e7c24c5c9
Barney Zinger <koshaevk@mail.ru>
2018-03-03 03:32:35 +0300

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,7 +1,7 @@
 """cipher of Caesar"""
 
 
-#english alphabet list
+#alphabet list
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 def confuse(letter, key):
